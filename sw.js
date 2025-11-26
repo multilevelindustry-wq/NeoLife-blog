a78c7a6cf78a629973dad6a55046c3af4f7b9b8f
multilevelindustry-wq <multilevelindustry@gmail.com>
2025-11-26 13:21:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10041064
+    "zoneId": 10239144
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw'
-
-    self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10101325
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
